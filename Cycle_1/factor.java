35d4dff8b54fa88b1a1d6da3b1a28b9338682c0f
Vyshnavi-V <85625961+Vyshnavi-V@users.noreply.github.com>
2022-02-26 10:09:00 +0530

    Create factor.java

---
+++ Cycle_1/factor.java
@@ -0,0 +1,11 @@
+class factor
+{
+public static void main(String args[])
+{
+int n=8;
+System.out.println("factorial of " +n+ "is");
+for(int i=1;i<=n;i++)
+{
+if(n%i==0)
+System.out.println(i +"");
+}}}
