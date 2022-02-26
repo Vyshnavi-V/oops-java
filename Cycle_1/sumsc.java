dbd4f7e8469d721c3998dbf94d3918da04d4bb8b
Vyshnavi-V <85625961+Vyshnavi-V@users.noreply.github.com>
2022-02-26 11:12:14 +0530

    Create sumsc.java

---
+++ Cycle_1/sumsc.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class add_two_numbers
+{
+public static void main(String args[])
+{
+int a,b;
+Scanner obj = new Scanner(System.in);
+System.out.println("Enter two numbers to ad :\n");
+a=obj.nextInt();
+b=obj.nextInt();
+System.out.println("sum is"+(a+b));
+}
+}
