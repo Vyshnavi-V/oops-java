7c8a7bf521ddc00a1a7c23d46e8837e9da3e7e2c
Vyshnavi-V <85625961+Vyshnavi-V@users.noreply.github.com>
2022-02-26 11:34:27 +0530

    Create oddeven2.java

---
+++ Cycle_7/oddeven2.java
@@ -0,0 +1,201 @@
+public class oddeven2{
+  
+    // Starting counter
+    int counter = 1;
+  
+    static int N=100;
+  
+    // Function to print odd numbers
+    public void printOddNumber()
+    {
+        synchronized (this)
+        {
+            // Print number till the N
+            while (counter < N) {
+  
+                // If count is even then print
+                while (counter % 2 == 0) {
+  
+                    // Exception handle
+                    try {
+                        wait();
+                    }
+                    catch (
+                        InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+  
+                // Print the number
+                System.out.print(counter + " ");
+  
+                // Increment counter
+                counter++;
+  
+                // Notify to second thread
+                notify();
+            }
+        }
+    }
+  
+    // Function to print even numbers
+    public void printEvenNumber()
+    {
+        synchronized (this)
+        {
+            // Print number till the N
+            while (counter < N) {
+  
+                // If count is odd then print
+                while (counter % 2 == 1) {
+  
+                    // Exception handle
+                    try {
+                        wait();
+                    }
+                    catch (
+                        InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+  
+                // Print the number
+                System.out.print(
+                    counter + " ");
+  
+                // Increment counter
+                counter++;
+  
+                // Notify to 2nd thread
+                notify();
+            }
+        }
+    }
+  
+    // Driver Code
+    public static void main(String[] args)
+    {
+        
+       oddeven2 mt = new oddeven2();
+  
+        // Create thread t1
+        Thread t1 = new Thread(new Runnable() {
+            public void run()
+            {
+                mt.printEvenNumber();
+            }
+        });
+  
+        // Create thread t2
+        Thread t2 = new Thread(new Runnable() {
+            public void run()
+            {public class oddeven2{
+  
+    // Starting counter
+    int counter = 1;
+  
+    static int N=100;
+  
+    // Function to print odd numbers
+    public void printOddNumber()
+    {
+        synchronized (this)
+        {
+            // Print number till the N
+            while (counter < N) {
+  
+                // If count is even then print
+                while (counter % 2 == 0) {
+  
+                    // Exception handle
+                    try {
+                        wait();
+                    }
+                    catch (
+                        InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+  
+                // Print the number
+                System.out.print(counter + " ");
+  
+                // Increment counter
+                counter++;
+  
+                // Notify to second thread
+                notify();
+            }
+        }
+    }
+  
+    // Function to print even numbers
+    public void printEvenNumber()
+    {
+        synchronized (this)
+        {
+            // Print number till the N
+            while (counter < N) {
+  
+                // If count is odd then print
+                while (counter % 2 == 1) {
+  
+                    // Exception handle
+                    try {
+                        wait();
+                    }
+                    catch (
+                        InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+  
+                // Print the number
+                System.out.print(
+                    counter + " ");
+  
+                // Increment counter
+                counter++;
+  
+                // Notify to 2nd thread
+                notify();
+            }
+        }
+    }
+  
+    // Driver Code
+    public static void main(String[] args)
+    {
+        
+       oddeven2 mt = new oddeven2();
+  
+        // Create thread t1
+        Thread t1 = new Thread(new Runnable() {
+            public void run()
+            {
+                mt.printEvenNumber();
+            }
+        });
+  
+        // Create thread t2
+        Thread t2 = new Thread(new Runnable() {
+            public void run()
+            {
+                mt.printOddNumber();
+            }
+        });
+  
+        // Start both threads
+        t1.start();
+        t2.start();
+    }
+}
+                mt.printOddNumber();
+            }
+        });
+  
+        // Start both threads
+        t1.start();
+        t2.start();
+    }
+}
